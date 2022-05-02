0afbf6634d310b0aa490c3d061011b4b83e09314
Murilo Rocha <software@murilorocha.com.br>
2022-05-02 19:28:49 -0300

    Teste do uso de variável com o tipo de dados byte em Java.

---
+++ src/TesteVariavelByte.java
@@ -0,0 +1,8 @@
+public class TesteVariavelByte {
+
+    public static void main(String[] args) {
+        byte quantidade = 22;
+
+        System.out.printf("A quantidade é: %d", quantidade);
+    }
+}
